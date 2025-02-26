c87e5c1485d0ad923aac85ef2c7ebdef234ec497
Václav Stodůlka <vsek@seznam.cz>
2025-02-26 13:13:25 +0100

    editorJs podpora seznamů

--- src/app/Component/EditorJs/EditorJsRenderer.php
+++ src/app/Component/EditorJs/EditorJsRenderer.php
@@ -20,8 +20,31 @@ class EditorJsRenderer
         foreach($jsonDecode['blocks'] as $block){
             switch ($block['type']){
                 case 'paragraph': $html->addHtml(Html::el('p')->setHtml($block['data']['text'])); break;
+                case 'list': (function() use ($block, $html){
+                    if($block['data']['style'] === 'unordered'){
+                        $mainTag = 'ul';
+                    }else{
+                        $mainTag = 'ol';
+                    }
+                    $htmlUl = Html::el($mainTag);
+                    $this->addListItems($mainTag, $htmlUl, $block['data']['items']);
+                    $html->addHtml($htmlUl);
+                })(); break;
                 default: throw new \Exception(sprintf('Block %s not supported', $block['type']));
             }
+        }
+        return $html;
+    }
+
+    private function addListItems(string $mainTag, Html $html, array $items):Html{
+        foreach($items as $item){
+            $li = Html::el('li')->setText($item['content']);
+            if(count($item['items']) > 0){
+                $subHtml = Html::el($mainTag);
+                $this->addListItems($mainTag, $subHtml, $item['items']);
+                $li->addHtml($subHtml);
+            }
+            $html->addHtml($li);
         }
         return $html;
     }
