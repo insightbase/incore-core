146a94c5b8cb6200529d9a4acdb39c50f9f2712d
Václav Stodůlka <vsek@seznam.cz>
2025-05-07 11:16:40 +0200

    content - možnost spravovat číselníky přímo v editaci obsahu

--- src/assets/admin/form.js
+++ src/assets/admin/form.js
@@ -1,3 +1,5 @@
+import naja from "naja";
+
 Array.from(document.getElementsByClassName('remove-fieldset')).forEach((element) => {
     element.addEventListener('click', function(event){
         event.preventDefault();
@@ -12,34 +14,39 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// najdeme na stránce všechny podřízené selectboxy
-document.querySelectorAll('select[data-depends]').forEach((childSelect) => {
-    let parentSelect = childSelect.form[childSelect.dataset.depends]; // nadřízený <select>
-    let url = childSelect.dataset.url; // atribut data-url
-    let items = JSON.parse(childSelect.dataset.items || 'null'); // atribut data-items
-
-    // když uživatel změní vybranou položku v nadřízeném selectu...
-    parentSelect.addEventListener('change', () => {
-        // pokud existuje atribut data-items...
-        if (items) {
-            // nahrajeme rovnou do podřízeného selectboxu nové položky
-            updateSelectbox(childSelect, items[parentSelect.value]);
-        }
-
-        // pokud existuje atribut data-url...
-        if (url) {
-            // uděláme AJAXový požadavek na endpoint s vybranou položkou místo placeholderu
-            fetch(url.replace(encodeURIComponent('#'), encodeURIComponent(parentSelect.value)))
-                .then((response) => response.json())
-                // a nahrajeme do podřízeného selectboxu nové položky
-                .then((data) => updateSelectbox(childSelect, data));
-        }
-    });
+naja.addEventListener('success', (event) => {
+   initForms();
 });
 
+initForms();
+function initForms() {
+// najdeme na stránce všechny podřízené selectboxy
+    document.querySelectorAll('select[data-depends]').forEach((childSelect) => {
+        let parentSelect = childSelect.form[childSelect.dataset.depends]; // nadřízený <select>
+        let url = childSelect.dataset.url; // atribut data-url
+        let items = JSON.parse(childSelect.dataset.items || 'null'); // atribut data-items
+
+        // když uživatel změní vybranou položku v nadřízeném selectu...
+        parentSelect.addEventListener('change', () => {
+            // pokud existuje atribut data-items...
+            if (items) {
+                // nahrajeme rovnou do podřízeného selectboxu nové položky
+                updateSelectbox(childSelect, items[parentSelect.value]);
+            }
+
+            // pokud existuje atribut data-url...
+            if (url) {
+                // uděláme AJAXový požadavek na endpoint s vybranou položkou místo placeholderu
+                fetch(url.replace(encodeURIComponent('#'), encodeURIComponent(parentSelect.value)))
+                    .then((response) => response.json())
+                    // a nahrajeme do podřízeného selectboxu nové položky
+                    .then((data) => updateSelectbox(childSelect, data));
+            }
+        });
+    });
+}
 // přepíše <options> v <select>
-function updateSelectbox(select, items)
-{
+function updateSelectbox(select, items) {
     select.innerHTML = ''; // odstraníme vše
     for (let id in items) { // vložíme nové
         let el = document.createElement('option');
