54701e494fdececae86a05d5c0aff1c798fa754d
Václav Stodůlka <vsek@seznam.cz>
2025-05-09 10:39:26 +0200

    content - custom renderer detail contentValue

--- src/assets/admin/content.js
+++ src/assets/admin/content.js
@@ -1,5 +1,9 @@
 import naja from "naja";
 
+naja.addEventListener('success', (event) => {
+    initBLockItemValueEdit();
+});
+
 let contentDetail = document.getElementsByClassName('contentDetail')[0];
 if (contentDetail) {
     let formLanguageSelect = contentDetail.getElementsByClassName('formLanguageSelect')[0];
@@ -9,4 +13,25 @@ if (contentDetail) {
             naja.makeRequest('GET', url)
         });
     }
+}
+
+initBLockItemValueEdit();
+function initBLockItemValueEdit() {
+    Array.from(document.getElementsByClassName('blockItem-value-edit')).forEach((element) => {
+        let input = element.getElementsByTagName('input')[0];
+        if (input) {
+            input.addEventListener('change', function () {
+                blockItemValueEdit(element, input.value);
+            });
+            input.addEventListener('keydown', function () {
+                if (event.key === 'Enter') {
+                    blockItemValueEdit(element, input.value);
+                }
+            });
+        }
+    });
+}
+function blockItemValueEdit(element, value){
+    let url = element.getAttribute('data-url-change').replace('xxxx', value);
+    naja.makeRequest('GET', url, {}, {history: false});
 }
